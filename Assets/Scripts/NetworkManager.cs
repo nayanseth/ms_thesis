ae2028730356a847b27f9bd2b1f6733f1653c5c6
Nayan Seth <nayantronix@gmail.com>
2016-09-18 19:34:20 -0700

    PUN Setup Complete

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+
+public class NetworkManager : PunBehaviour
+{
+
+    const string VERSION = "0.0.1";
+
+    // Use this for initialization
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionState.ToString());
+    }
+
+    void OnJoinLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't Join Random Room....Creating a new Room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        // Do nothing
+    }
+}
