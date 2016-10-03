c8ff9312826c6cc1a5f7a9bb0480a3ed62529ec2
Nayan Seth <nayantronix@gmail.com>
2016-10-02 23:32:06 -0700

    Added laser pointer from controllers

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour
+{
+
+    LineRenderer line;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = true;
+        line.useWorldSpace = false;
+        line.SetVertexCount(2);
+        line.SetColors(Color.cyan, Color.cyan);
+        line.SetWidth(0.01f, 0.01f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            print("Collider hit: " + hit.collider);
+            if (hit.collider)
+            {
+                print("Cube found at " + hit.point + "with normal " + hit.normal);
+                line.SetPosition(1, new Vector3(transform.rotation.x, 0, hit.distance));
+            }
+        }
+        else
+        {
+            print("No Collider");
+            line.SetPosition(1, new Vector3(transform.rotation.x, 0, 10000));
+        }
+    }
+}
