a427a47ed42c1d048ca8abb210887cc1997c3e1c
Nayan Seth <nayantronix@gmail.com>
2016-10-18 15:23:43 -0700

    Added new Skybox and Scene Controller!

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	public int counter;
+
+	// Use this for initialization
+	void Start () {
+
+		counter = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
